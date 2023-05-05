c2c6b9f1bd5d02420109a7ca68616a87de98047d
Pretlove Web Design <132574554+Pretlovewebdesign@users.noreply.github.com>
2023-05-05 12:53:20 +1000

    Add files via upload

---
+++ Js/Simplicite.js
@@ -0,0 +1,36 @@
+// Mobile
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+// Slider
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1} 
+  x[slideIndex-1].style.display = "block"; 
+  setTimeout(carousel, 9000); 
+}
+
+
+
+
+
+// Counting numbers up
+var clicks = 0 ;
+    function onClick() {	
+        clicks += 1;
+        document.getElementById("clicks").innerHTML = clicks;
+    };
